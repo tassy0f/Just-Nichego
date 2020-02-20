aff77fc3446612b8174f1f7abdd2279a067a1b98
tassy0f <46904048+tassy0f@users.noreply.github.com>
2020-02-21 01:54:46 +0300

    Initial commit

---
+++ app/src/main/java/com/example/justnichego/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.justnichego;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.text.NumberFormat;
+
+/**
+ * This app displays an order form to order coffee.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public int i = 0;
+    public int g = 50;
+    public void plus(View view) {
+        i++;
+        display(i);
+        displayPrice(i * g);
+    }
+
+    public void minus(View view) {
+        i--;
+        display(i);
+        displayPrice(i * g);
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+}
